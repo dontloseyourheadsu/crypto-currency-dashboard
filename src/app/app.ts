85cac25c5830e0ce65f4c69e7747d49366c799aa
dontloseyourheadsu <dontloseyourheadsu@gmail.com>
2025-07-17 15:01:15 -0600

    app routing simplify

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavMenu } from "./shared/layout/nav-menu/nav-menu";
+import { Mainlayout } from "./shared/layout/mainlayout/mainlayout/mainlayout";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavMenu, Mainlayout],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
