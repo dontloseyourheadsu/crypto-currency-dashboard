32d6232bbcad6edb780420b5709a8b02eb1ff7e8
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-07-19 15:09:59 -0600

    nav menu update to use link components

--- src/app/shared/layout/nav-menu/nav-menu.ts
+++ src/app/shared/layout/nav-menu/nav-menu.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { NavLink } from '../../inputs/nav-link/nav-link';
 
 @Component({
   selector: 'app-nav-menu',
-  imports: [],
+  imports: [NavLink],
   templateUrl: './nav-menu.html',
   styleUrl: './nav-menu.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class NavMenu {
 
