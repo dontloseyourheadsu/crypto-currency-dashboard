c050de007a85ad8d3d1464d880e9491bac7e3498
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-07-20 17:35:41 -0600

    nav use material

--- src/app/shared/layout/nav-menu/nav-menu.ts
+++ src/app/shared/layout/nav-menu/nav-menu.ts
@@ -1,13 +1,39 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { NavLink } from '../../inputs/nav-link/nav-link';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-menu',
-  imports: [NavLink],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterModule,
+    AsyncPipe,
+    CommonModule
+  ],
   templateUrl: './nav-menu.html',
   styleUrl: './nav-menu.css',
   encapsulation: ViewEncapsulation.None,
 })
 export class NavMenu {
+  isHandset$: Observable<boolean>;
 
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.isHandset$ = this.breakpointObserver.observe(Breakpoints.Handset)
+      .pipe(
+        map(result => result.matches),
+        shareReplay()
+      );
+  }
 }
